533d4553468eb4b6ef7d152d67c36df92dde86e3
jacoder <mantle@vivaldi.net>
2024-02-13 06:21:59 -0500

    IMPORT VUE APP
    Imported the edugress Vue ap into index.html template, and displayed two
    data properties.

--- 1_create_vue_app/main.js
+++ 1_create_vue_app/main.js
@@ -1,7 +1,12 @@
+// define Vue app called edugress.
 const  edugress = Vue.createApp({
-    data(){
+
+    // define data properties
+    data()
+    {
         return{
             school: 'Ferncourt Hight',
+            address: 'Claremount, St Ann'
         }
     }
 })
